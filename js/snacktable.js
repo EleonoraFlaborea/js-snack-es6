572a68bc774f1e190f1a6e31d3fedcb6397200bd
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-01-16 15:01:55 +0100

    add array with name, seat, name table

--- js/snacktable.js
+++ js/snacktable.js
@@ -17,3 +17,35 @@ const guests = [
   'Maneskin'
 ];
 */
+
+
+const tableName = 'Tavolo Vip';
+
+const guests = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+  ];
+
+
+
+// ARRAY CON I NOMI DEI VIP
+const vipTable = [];
+
+guests.forEach((guest, i)=> {
+    console.log('Name: ',guest)
+    console.log('Seats: ', i)
+    console.log('Table: ', tableName)
+    
+})
+
+
+
+
