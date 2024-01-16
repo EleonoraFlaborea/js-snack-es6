e795f87291e16d1bcc54bedd932e538d1ccb6d92
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-01-16 15:08:41 +0100

    add array of objects

--- js/snackstudents.js
+++ js/snackstudents.js
@@ -17,4 +17,45 @@ Id|Name|Grades
  */
 
 
+const students = [ 
+  { id:  213,
+    name: 'Marco della Rovere',
+    grades: 78,
+  },
 
+  { id:  110,
+    name: 'Paola Cortellessa',
+    grades: 96,
+  }, 
+
+  { id: 250 ,
+    name: 'Andrea Mantegna',
+    grades: 48 ,
+  },
+
+  { id: 145 ,
+    name: 'Gaia Borromini' ,
+    grades: 74,
+  },
+
+  { id: 196 ,
+    name: 'Luigi Grimaldello',
+    grades: 68,
+  },
+
+  { id: 102 ,
+    name: 'Piero della Francesca' ,
+    grades: 50,
+  },
+
+  { id: 120 ,
+    name: 'Francesca da Polenta',
+    grades: 84,
+  },
+]
+
+
+// TODO: 
+/*1. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+2.Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+3.  dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo ES (Marco della Rovere => MARCO DELLA ROVERE);*/
