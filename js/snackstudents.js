6042e6ed387c5cc98875d16217c89c0cfb6413e7
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-01-16 14:33:02 +0100

    add two files js

--- js/snackstudents.js
+++ js/snackstudents.js
@@ -1,37 +1,3 @@
-/*Ciao ragazzi,
-esercizio di oggi: Snack Array Methods
-nome repo: js-snack-es6
-SNACK 1
-Il Grande Gatsby ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
-Ci ha lasciato il nome del tavolo  e la lista degli invitati in ordine di posto.
-const tableName = 'Tavolo Vip';
-
-const guests = [
-  'Brad Pitt',
-  'Johnny Depp',
-  'Lady Gaga',
-  'Cristiano Ronaldo',
-  'Georgina Rodriguez',
-  'Chiara Ferragni',
-  'Fedez',
-  'George Clooney',
-  'Amal Clooney',
-  'Maneskin'
-];
-Per stampare il tutto,  la tipografia  vuole che le mandiamo una lista di ospiti in cui ogni ospite sia un oggetto javascript che ha come attributi: nome del tavolo, nome dell'ospite e posto occupato.
-Generiamo questo array in una variabile guestsList e stampiamolo in console */
-
-
-
-
-
-
-
-
-
-
-
-
 /*
 SNACK 2
 Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
@@ -48,4 +14,7 @@ Id|Name|Grades
 196|Luigi Grimaldello|68
 102|Piero della Francesca|50
 120|Francesca da Polenta|84
-*/
+ */
+
+
+
